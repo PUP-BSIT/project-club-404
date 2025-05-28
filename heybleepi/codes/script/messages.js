772befda455d8000f7faf81ed346dff4cc84cf75
ynion-mabeamae <mabeamaeynion@gmail.com>
2025-05-28 20:13:25 +0800

    fix(messages): remove the alert from messages.js

--- heybleepi/codes/script/messages.js
+++ heybleepi/codes/script/messages.js
@@ -8,20 +8,18 @@ document.querySelectorAll(".comment-edit").forEach(function (span) {
 // Delete action
 document.querySelectorAll(".comment-delete").forEach(function (span) {
   span.addEventListener("click", function () {
-    if (confirm("Delete this comment?")) {
-      this.previousElementSibling.submit();
-    }
+    this.previousElementSibling.submit();
   });
 });
 
 // Show selected image or file names
-document.getElementById('imageInput').addEventListener('change', function () {
+document.getElementById("imageInput").addEventListener("change", function () {
   if (this.files.length > 0) {
     alert("Selected image: " + this.files[0].name);
   }
 });
 
-document.getElementById('fileInput').addEventListener('change', function () {
+document.getElementById("fileInput").addEventListener("change", function () {
   if (this.files.length > 0) {
     alert("Selected file: " + this.files[0].name);
   }
