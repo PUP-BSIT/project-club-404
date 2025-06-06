5441fcd90dfa36004632c201e990da0a563e5978
ynion-mabeamae <mabeamaeynion@gmail.com>
2025-06-06 20:37:09 +0800

    fix(messages): remove file selection alerts.

--- heybleepi/codes/script/messages.js
+++ heybleepi/codes/script/messages.js
@@ -11,16 +11,3 @@ document.querySelectorAll(".comment-delete").forEach(function (span) {
     this.previousElementSibling.submit();
   });
 });
-
-// Show selected image or file names
-document.getElementById("imageInput").addEventListener("change", function () {
-  if (this.files.length > 0) {
-    alert("Selected image: " + this.files[0].name);
-  }
-});
-
-document.getElementById("fileInput").addEventListener("change", function () {
-  if (this.files.length > 0) {
-    alert("Selected file: " + this.files[0].name);
-  }
-});
