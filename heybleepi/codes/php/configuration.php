610ff193beaffc40aa2fc79d9ece1f6ef6edf9f8
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2025-06-22 15:04:40 +0800

    feat(configuration): change timezone to Philippines
    Changed the timezone into Philippines (Asia/Manila) in the configuration file so that it will give a accurate time for token creation and expiration.

--- heybleepi/codes/php/configuration.php
+++ heybleepi/codes/php/configuration.php
@@ -1,14 +1,22 @@
 <?php
-$database = "localhost";
+// Set PHP timezone for date() and time() functions
+date_default_timezone_set('Asia/Manila');
+
+$database = "srv678.hstgr.io";
 $username = "u937067793_club_404_mem";
 $password = "Club-404-!_!";
 $dbname = "u937067793_club_404";
 
-// Create connection
+// Create MySQL connection
 $conn = new mysqli($database, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+}
+
+// Set MySQL session timezone to Asia/Manila (UTC+8)
+if (!$conn->query("SET time_zone = '+08:00'")) {
+    die("Failed to set MySQL timezone: " . $conn->error);
 }
 ?>
