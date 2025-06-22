8beced9a45a52cac865693c4d9c13e97ef09cb72
delima-justine <delimajustine24@gmail.com>
2025-06-22 13:25:21 +0800

    refactor(form): remove tailwind

--- heybleepi/codes/php/index.php
+++ heybleepi/codes/php/index.php
@@ -48,8 +48,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>HeyBleepi - Connect in the Galaxy</title>
-
-    <script src="https://cdn.tailwindcss.com/3.4.16"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.6.0/remixicon.min.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
