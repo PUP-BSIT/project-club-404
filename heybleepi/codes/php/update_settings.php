7e51adcf951166ecb3a530035e9ad9f27e8fbb60
delima-justine <delimajustine24@gmail.com>
2025-07-12 02:10:57 +0800

    changes(settings): add verify email if existing.

--- heybleepi/codes/php/update_settings.php
+++ heybleepi/codes/php/update_settings.php
@@ -33,32 +33,62 @@ if ($stmt->num_rows > 0) {
 }
 $stmt->close();
 
-// Update information query
-$sql = "UPDATE users 
-        SET user_name = '{$updatedUsername}', 
-        first_name = '{$updatedFirstName}', 
-        middle_name = '{$updatedMiddleName}', 
-        last_name = '{$updatedLastName}',
-        email = '{$updatedEmail}', 
-        birthdate = '{$updatedBirthdate}' 
-        WHERE id = '{$currentId}';
-        
-        UPDATE messages 
-        SET user_name = '{$updatedUsername}'
-        WHERE user_id = '{$currentId}';
-    ";
-      
-// Handle error.
-if(!mysqli_multi_query($conn, $sql)) {
-  echo "Failed to update account information";
+// Check for existing email
+$stmt = $conn->prepare("SELECT id FROM users WHERE email = ? AND id != ?");
+$stmt->bind_param("si", $updatedEmail, $currentId);
+$stmt->execute();
+$stmt->store_result();
+if ($stmt->num_rows > 0) {
+   echo "Email already taken!";
+   $stmt->close();
+   $conn->close();
+   exit();
 }
+$stmt->close();
 
+// Update information query
+$stmt1 = $conn->prepare("UPDATE users 
+                    SET user_name = ?, 
+                    first_name = ?, 
+                    middle_name = ?, 
+                    last_name = ?, 
+                    email = ?, 
+                    birthdate = ? 
+                    WHERE id = ?");
+            
+$stmt1->bind_param("ssssssi", 
+                $updatedUsername, 
+                $updatedFirstName, 
+                $updatedMiddleName, 
+                $updatedLastName, 
+                $updatedEmail, 
+                $updatedBirthdate, 
+                $currentId);
+
+if (!$stmt1->execute()) {
+    echo "Failed to update users table: " . $stmt1->error;
+    $stmt1->close();
+    $conn->close();
+    exit();
+}
+$stmt1->close();
+
+// Update username in messages.
+$stmt2 = $conn->prepare("UPDATE messages SET user_name = ? WHERE user_id = ?");
+$stmt2->bind_param("si", $updatedUsername, $currentId);
+if (!$stmt2->execute()) {
+    echo "Failed to update messages table: " . $stmt2->error;
+    $stmt2->close();
+    $conn->close();
+    exit();
+}
+$stmt2->close();
+echo "Changes Saved!";
+
+// Update session information
 $_SESSION['username'] = $updatedUsername;
 $_SESSION['email'] = $updatedEmail;
 $_SESSION['first_name'] = $updatedFirstName;
 $_SESSION['middle_name'] = $updatedMiddleName;
 $_SESSION['last_name'] = $updatedLastName;
-
-echo "Changes Saved!";
-mysqli_close($conn);
 ?>
